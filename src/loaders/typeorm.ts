1731a4cf56ae60923e0e5bc018866e37e69bffac
Marine Incera <marine.incera@gmail.com>
2020-07-17 16:30:57 +0200

    loaders: token entity added to the list

--- src/loaders/typeorm.ts
+++ src/loaders/typeorm.ts
@@ -1,37 +1,36 @@
-
-import { createConnection } from 'typeorm';
-import { Category } from '../entities/category.entity';
-import { Evaluation } from '../entities/evaluation.entity';
-import { Item } from '../entities/item.entity';
-import { ItemStatus } from '../entities/item-status.entity';
-import { Loan } from '../entities/loan.entity';
-import { LoanStatus } from '../entities/loan-status.entity';
-import { Opinion } from '../entities/opinion.entity';
-import { Tag } from '../entities/tag.entity';
-import { User } from '../entities/user.entity';
-import { Console } from '../entities/console.entity';
+import { createConnection } from "typeorm";
+import { Category } from "../entities/category.entity";
+import { Evaluation } from "../entities/evaluation.entity";
+import { Item } from "../entities/item.entity";
+import { ItemStatus } from "../entities/item-status.entity";
+import { Loan } from "../entities/loan.entity";
+import { LoanStatus } from "../entities/loan-status.entity";
+import { Opinion } from "../entities/opinion.entity";
+import { Tag } from "../entities/tag.entity";
+import { User } from "../entities/user.entity";
+import { Console } from "../entities/console.entity";
+import { Token } from "src/entities/token.entity";
 
 export default async () => {
-
-await createConnection({
-    type: 'mysql',
+  await createConnection({
+    type: "mysql",
     port: 3306,
-    username: 'root',
-    password: process.env.BORROW_DB_PASS, 
+    username: "root",
+    password: process.env.BORROW_DB_PASS,
     database: process.env.BORROW_DB_DATABASE,
     entities: [
-        Category,
-        Console,
-        Evaluation,
-        Item,
-        ItemStatus,
-        Loan,
-        LoanStatus,
-        Opinion,
-        Tag,
-        User
-
+      Category,
+      Console,
+      Evaluation,
+      Item,
+      ItemStatus,
+      Loan,
+      LoanStatus,
+      Opinion,
+      Tag,
+      Token,
+      User,
     ],
     synchronize: true,
-});
+  });
 };
