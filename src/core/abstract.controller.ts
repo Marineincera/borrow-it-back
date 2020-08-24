a04c0fc44ac1d8acdcf0ce4ef6a63a4b1d2e88b4
Marine Incera <marine.incera@gmail.com>
2020-08-24 08:27:03 +0200

    feat(items-search): update

--- src/core/abstract.controller.ts
+++ src/core/abstract.controller.ts
@@ -1,36 +1,37 @@
-import express, { Request, Response, Router } from 'express';
-import { AbstractService } from './abstract.service';
+import express, { Request, Response, Router } from "express";
+import { AbstractService } from "./abstract.service";
 
 /**
- * Ce controller vous servira de modèle pour construire vos différent controller
  * Le controller est la partie de l'application qui est en charge de la reception
  * des requetes http.
  *
- * @param app l'application express
+ * @param app
  */
-export const commonController = (service: AbstractService, commonRouter = Router()) => {
+export const commonController = (
+  service: AbstractService,
+  commonRouter = Router()
+) => {
+  commonRouter.get("/", async (req: Request, res: Response) => {
+    res.send(await service.getAll());
+  });
 
-    commonRouter.get('/', async (req: Request, res: Response) => {
-        res.send(await service.getAll());
-    });
+  commonRouter.get("/:id", async (req: Request, res: Response) => {
+    res.send(await service.getById(Number(req.params.id)));
+  });
 
-    commonRouter.get('/:id', async (req: Request, res: Response) => {
-        res.send(await service.getById(Number(req.params.id)));
-    });
+  commonRouter.post("/", async (req: Request, res: Response) => {
+    res.send(await service.add(req.body));
+  });
 
-    commonRouter.post('/', async (req: Request, res: Response) => {
-        res.send(await service.add(req.body));
-    });
+  commonRouter.put("/update/:id", async (req: Request, res: Response) => {
+    await service.update(parseInt(req.params.id, 10), req.body);
+    res.send(204);
+  });
 
-    commonRouter.put('/update/:id', async (req: Request, res: Response) => {
-        await service.update(parseInt(req.params.id, 10), req.body);
-        res.send(204);
-    });
+  commonRouter.delete("/delete/:id", async (req: Request, res: Response) => {
+    await service.delete(parseInt(req.params.id, 10));
+    res.sendStatus(204);
+  });
 
-    commonRouter.delete('/delete/:id', async (req: Request, res: Response) => {
-        await service.delete(parseInt(req.params.id, 10));
-        res.sendStatus(204);
-    });
-
-    return commonRouter;
+  return commonRouter;
 };
